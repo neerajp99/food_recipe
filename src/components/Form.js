91a2a7a69dbfda539ead3403e389a2aac0918145
neerajp99 <neerajp1999@gmail.com>
2019-01-02 01:58:13 +0530

    add class to recipies

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,14 +1,20 @@
 import React, { Component } from "react";
 
-
-
-const Form = props => (
-  <form onSubmit = {props.getRecipe} style={{
-    'marginBottom': "2rem",
-  }}>
-    <input className="recipe_form" type="text" name="recipeName" />
-    <button className="recipe_form_button"> Submit </button>
-  </form>
-);
+class Form extends Component {
+  render() {
+    const { getRecipe } = this.props;
+    return (
+      <form
+        onSubmit={getRecipe}
+        style={{
+          marginBottom: "2rem"
+        }}
+      >
+        <input className="recipe_form" type="text" name="recipeName" />
+        <button className="recipe_form_button"> Submit </button>
+      </form>
+    );
+  }
+}
 
 export default Form;
